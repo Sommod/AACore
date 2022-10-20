fddc93781ca64645388b23b9739b14b8312693dd
BuildTools <unconfigured@null.spigotmc.org>
2022-10-19 22:01:47 -0400

    Added static methods

--- src/main/java/coldfyre/aacore/api/Utilities.java
+++ src/main/java/coldfyre/aacore/api/Utilities.java
@@ -18,5 +18,48 @@ public class Utilities {
 		
 		return result;
 	}
+	
+	public static String[] wordWarpAsArray(String input, int words) {
+		return wordWarp(input, words).split("\n");
+	}
+	
+	public static String[] addToArray(String[] input, String value) {
+		return addToArray(input.length, input, value);
+	}
+	
+	public static String[] addToArray(int element, String[] input, String value) {
+		if(element < 0 || element >= (input.length + 1))
+			throw new ArrayIndexOutOfBoundsException("Error, tried to edit String array by inserting value into element beyond array size.");
+		
+		String[] result = new String[input.length + 1];
+		
+		for(int i = 0, j = 0; i < result.length; i++) {
+			if(i == element) {
+				result[i] = value;
+				j++;
+				continue;
+			}
+			
+			result[i] = input[i - j];
+		}
+		
+		return result;
+	}
+	
+	public static String[] removeFromArray(String[] input, int element) {
+		if(element < 0 || element >= input.length)
+			throw new ArrayIndexOutOfBoundsException("Error, tried to edit String array by removing value from element beyond array size.");
+		
+		String[] result = new String[input.length - 1];
+		
+		for(int i = 0, j = 0; i < result.length; i++) {
+			if(i == element)
+				j++;
+			
+			result[i] = input[i + j];
+		}
+		
+		return result;
+	}
 
 }
