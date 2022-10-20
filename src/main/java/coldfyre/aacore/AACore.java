b8a17d8963b4a2aa485cd396e72852e8ce6b1347
BuildTools <unconfigured@null.spigotmc.org>
2022-10-19 21:01:41 -0400

    Initialized PluginManager class

--- src/main/java/coldfyre/aacore/AACore.java
+++ src/main/java/coldfyre/aacore/AACore.java
@@ -5,6 +5,13 @@ package coldfyre.aacore;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import coldfyre.aacore.api.manager.PluginManager;
+
 public class AACore extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		new PluginManager<AACore>(this);
+	}
 
 }
